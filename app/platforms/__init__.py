3d023e4671276486a59af6aaf1f70249d0f8ef85
hr222 <pengyulong123@yeah.net>
2026-07-14 08:32:49 +0800

    新增乐有家

--- app/platforms/__init__.py
+++ app/platforms/__init__.py
@@ -5,5 +5,12 @@ from app.platforms.ajk import AjkPlatformAdapter
 from app.platforms.base import PlatformAdapter
 from app.platforms.fang import FangPlatformAdapter
 from app.platforms.ke import KePlatformAdapter
+from app.platforms.lyj import LyjPlatformAdapter
 
-__all__ = ["PlatformAdapter", "KePlatformAdapter", "AjkPlatformAdapter", "FangPlatformAdapter"]
+__all__ = [
+    "PlatformAdapter",
+    "KePlatformAdapter",
+    "AjkPlatformAdapter",
+    "FangPlatformAdapter",
+    "LyjPlatformAdapter",
+]
